2431979e247d72071448c453679c6343286c1470
Someshwaran <somesh599485@gmail.com>
2017-02-01 23:40:40 +0530

    Create collector.java

---
+++ java/collector.java
@@ -0,0 +1,90 @@
+package com.org;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class Collector
+ */
+@WebServlet("/infor")
+public class Collector extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Collector() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	 
+		try(PrintWriter out = response.getWriter())
+		{
+			response.setContentType("text/html");
+
+			//out.println("hello");
+			try{
+				String username = request.getParameter("username");
+				 
+			try{
+					Class.forName("com.mysql.jdbc.Driver");
+			}
+			catch(ClassNotFoundException r){
+				r.printStackTrace();
+			}
+				Connection c= DriverManager.getConnection("jdbc:mysql://localhost:3306/test","","");
+				Statement st= c.createStatement();
+				ResultSet rs=st.executeQuery("select * from login where user='"+username+"'");
+				if(rs.next())
+				{		System.out.println("Entered inservlet ");
+					 	response.sendRedirect("welcome.html");
+										
+				}
+				else
+				{
+					out.println("<html><head></head><body onload=\"alert('Enter correct Password')\"></body></html>");
+					out.println();
+					//alert("Enter the correct password");
+					//response.sendRedirect("index.html");
+				}
+				rs.close();
+				c.close();
+			
+			
+		}
+			catch(Exception e)
+			{
+				System.out.println(e);
+			}
+		}
+		catch(Exception e){
+			System.out.println(e);
+		}
+
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
